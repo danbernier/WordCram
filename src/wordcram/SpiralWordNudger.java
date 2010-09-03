00966d64176ebf0c7a1b819582b5e82410c0e67b
danbernier <danbernier@db2b9112-8060-c212-668a-ea1c16feaf2a>
2010-09-03 17:00:15 +0000

    for SpiralWordNudger, tossed the archimedian crap -- just use regular radius incrementing, it works out about the same, but starts at the CENTER

--- src/wordcram/SpiralWordNudger.java
+++ src/wordcram/SpiralWordNudger.java
@@ -21,26 +21,18 @@ import processing.core.PVector;
 
 public class SpiralWordNudger implements WordNudger {
 
-	private float a;
-	private float b;
-	
 	// Who knows? this seems to be good, but it seems to depend on the font -- bigger fonts need a bigger thetaIncrement.
 	private float thetaIncrement = (float)(Math.PI * 0.03);
 
-	public SpiralWordNudger() {
-		this(1, 1);
-	}
-
-	public SpiralWordNudger(float _a, float _b) {
-		a = _a;
-		b = _b;
-	}
-
 	@Override
 	public PVector nudgeFor(Word w, int attempt) {
-		b = PApplet.map(attempt, 0, 600, 1, 10);
+		float rad = PApplet.map(attempt, 0, 600, 1, 50);
+		
+		if (attempt > 400) {
+			rad += attempt - 400;
+		}
+		
 		float theta = thetaIncrement * attempt;
-		float rad = a + b * theta;
 		float x = PApplet.cos(theta) * rad;
 		float y = PApplet.sin(theta) * rad;
 		return new PVector(x, y);
