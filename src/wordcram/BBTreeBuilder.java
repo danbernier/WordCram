a55035160463da50c20e8c57ece6d27e01b62fff
danbernier <danbernier@db2b9112-8060-c212-668a-ea1c16feaf2a>
2010-10-19 01:46:53 +0000

    BBTreeBuilder: removed a bunch of dead code, and improved variable names

--- src/wordcram/BBTreeBuilder.java
+++ src/wordcram/BBTreeBuilder.java
@@ -24,84 +24,43 @@ import processing.core.PImage;
 class BBTreeBuilder {
 	public BBTree makeTree(Shape shape, int minBoxSize) {
 		Rectangle2D bounds = shape.getBounds2D();
-		int x = (int) bounds.getX();
-		int y = (int) bounds.getY();
-		int x2 = x + (int) bounds.getWidth();
-		int y2 = y + (int) bounds.getHeight();
-		return makeTree(shape, minBoxSize, x, y, x2, y2);
+		int left = (int) bounds.getX();
+		int top = (int) bounds.getY();
+		int right = left + (int) bounds.getWidth();
+		int bottom = top + (int) bounds.getHeight();
+		return makeTree(shape, minBoxSize, left, top, right, bottom);
 	}
 
-	private BBTree makeTree(Shape shape, int minBoxSize, int x1, int y1,
-			int x2, int y2) {
+	private BBTree makeTree(Shape shape, int minBoxSize, int left, int top,
+			int right, int bottom) {
 
-		boolean intersects = shape.intersects(x1, y1, x2 - x1, y2 - y1);
-		boolean contains = shape.contains(x1, y1, x2 - x1, y2 - y1);
+		int width = right - left;
+		int height = bottom - top;
+		boolean intersects = shape.intersects(left, top, width, height);
+		boolean contains = shape.contains(left, top, width, height);
 
 		if (!intersects && !contains) {
 			return null;
 		} else {
-			BBTree tree = new BBTree(x1, y1, x2, y2);
+			BBTree tree = new BBTree(left, top, right, bottom);
 			if (!contains) {
-				boolean smallEnoughToStop = x2 - x1 <= minBoxSize;
+				boolean smallEnoughToStop = width <= minBoxSize;
 				if (!smallEnoughToStop) {
-					int newX = avg(x1, x2);
-					int newY = avg(y1, y2);
+					int centerX = avg(left, right);
+					int centerY = avg(top, bottom);
 
 					// upper left
-					BBTree t0 = makeTree(shape, minBoxSize, x1, y1, newX, newY);
+					BBTree t0 = makeTree(shape, minBoxSize, left, top, centerX,
+							centerY);
 					// upper right
-					BBTree t1 = makeTree(shape, minBoxSize, newX, y1, x2, newY);
+					BBTree t1 = makeTree(shape, minBoxSize, centerX, top,
+							right, centerY);
 					// lower left
-					BBTree t2 = makeTree(shape, minBoxSize, x1, newY, newX, y2);
+					BBTree t2 = makeTree(shape, minBoxSize, left, centerY,
+							centerX, bottom);
 					// lower right
-					BBTree t3 = makeTree(shape, minBoxSize, newX, newY, x2, y2);
-
-					tree.addKids(t0, t1, t2, t3);
-				}
-			}
-
-			return tree;
-		}
-	}
-		  
-		  
-	public BBTree makeTree(PImage buffer, int bgColor, int minBoxSize) {
-		return makeTree(buffer, bgColor, minBoxSize, 0, 0, buffer.width,
-				buffer.height);
-	}
-
-	private BBTree makeTree(PImage buffer, int bgColor, int minBoxSize, int x1,
-			int y1, int x2, int y2) {
-
-		int colorInfo = getColorInfo(buffer, bgColor, x1, y1, x2, y2);
-
-		// boolean hasBgColor = (colorInfo & 1) > 0;
-		// boolean hasFill = (colorInfo & 2) > 0;
-		boolean hasBgColor = colorInfo == 3 || colorInfo == 1;
-		boolean hasFill = colorInfo == 3 || colorInfo == 2;
-
-		if (!hasFill) {
-			return null;
-		} else {
-			BBTree tree = new BBTree(x1, y1, x2, y2);
-			if (hasBgColor) {
-				boolean smallEnoughToStop = x2 - x1 <= minBoxSize;
-				if (!smallEnoughToStop) {
-					int newX = avg(x1, x2);
-					int newY = avg(y1, y2);
-
-					// upper left
-					BBTree t0 = makeTree(buffer, bgColor, minBoxSize, x1, y1,
-							newX, newY);
-					// upper right
-					BBTree t1 = makeTree(buffer, bgColor, minBoxSize, newX, y1,
-							x2, newY);
-					// lower left
-					BBTree t2 = makeTree(buffer, bgColor, minBoxSize, x1, newY,
-							newX, y2);
-					// lower right
-					BBTree t3 = makeTree(buffer, bgColor, minBoxSize, newX,
-							newY, x2, y2);
+					BBTree t3 = makeTree(shape, minBoxSize, centerX, centerY,
+							right, bottom);
 
 					tree.addKids(t0, t1, t2, t3);
 				}
@@ -114,42 +73,6 @@ class BBTreeBuilder {
 	private int avg(int a, int b) {
 		// reminder: x >> 1 == x / 2
 		// avg = (a+b)/2 = (a/2)+(b/2) = (a>>1)+(b>>1)
-		return (a >> 1) + (b >> 1);
-	}
-
-	/*
-	 * Has one job: find whether the region contains a) any bgColor, and b) any
-	 * OTHER color. If it has NO bgColor, we can stop recursing. If it has ONLY
-	 * bgColor, we can stop recursing. Returns a 2-bit int, basically: lowest
-	 * bit being hasBgColor, highest bit being hasOtherColor: 0 = neither
-	 * (impossible, really) 1 = has bgColor only 2 = has no bgColor, only other
-	 * color(s) 3 = has both bgColor and some other color
-	 */
-	private int getColorInfo(PImage buffer, int bgColor, int x1, int y1,
-			int x2, int y2) {
-
-		boolean seenBgColor = false;
-		boolean seenOtherColor = false;
-
-		int yOff = y1 * buffer.width;
-
-		for (int y = y1; y < y2; y++) {
-			int xStart = yOff + x1;
-			int xStop = yOff + x2;
-			for (int i = xStart; i < xStop; i++) {
-				if (buffer.pixels[i] == bgColor) {
-					seenBgColor = true;
-				} else {
-					seenOtherColor = true;
-				}
-				if (seenBgColor && seenOtherColor) {
-					return 3; // short-circuit
-				}
-			}
-			yOff += buffer.width;
-		}
-
-		// last case should never happen
-		return seenBgColor ? 1 : seenOtherColor ? 2 : 0;
+		return (a + b) >> 1;
 	}
 }
