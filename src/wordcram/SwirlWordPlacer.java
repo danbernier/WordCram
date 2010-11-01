c166dd4276e234b5812d3e619ec9a4ca96fe6193
danbernier <danbernier@db2b9112-8060-c212-668a-ea1c16feaf2a>
2010-11-01 00:13:59 +0000

    cleaned up Swirl & Wave wordplacers -- they work the same, they're just cleaner

--- src/wordcram/SwirlWordPlacer.java
+++ src/wordcram/SwirlWordPlacer.java
@@ -1,51 +1,41 @@
 package wordcram;
 
 /*
-Copyright 2010 Daniel Bernier
+ Copyright 2010 Daniel Bernier
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
-import processing.core.PApplet;
-import processing.core.PVector;
+import processing.core.*;
 
 public class SwirlWordPlacer implements WordPlacer {
 
-	private java.util.Random r = new java.util.Random();
-	
 	@Override
-	public PVector place(Word word, int wordIndex, int wordsCount, int wordImageWidth, int wordImageHeight, int fieldWidth, int fieldHeight) {
+	public PVector place(Word word, int wordIndex, int wordsCount,
+			int wordImageWidth, int wordImageHeight, int fieldWidth,
+			int fieldHeight) {
 
-		float theta = PApplet.map(wordIndex, 0, wordsCount, 0, 6 * PApplet.TWO_PI);
-		
-		// TODO applet.width/2 -> max = triggy, based on sin/cos, so if height is short, it stays onscreen 
-		float radius = PApplet.map(wordIndex, 0, wordsCount, 0, fieldWidth/2);
-//		float radius = PApplet.map((float)word.weight, 0, 1.0f, 0, applet.width/2);
-//		radius = radius * (float)word.weight * 100;
-		
-		float cx = fieldWidth/2;
-		float cy = fieldHeight/2;
-		
+		float normalizedIndex = (float) wordIndex / wordsCount;
+
+		float theta = normalizedIndex * 6 * PConstants.TWO_PI;
+		float radius = normalizedIndex * fieldWidth / 2f;
+
+		float centerX = fieldWidth * 0.5f;
+		float centerY = fieldHeight * 0.5f;
+
 		float x = PApplet.cos(theta) * radius;
 		float y = PApplet.sin(theta) * radius;
-		
-		x += cx;
-		y += cy;
-		
-		//x += r.nextGaussian() * 20;
-		//y += r.nextGaussian() * 20;
-		
-		return new PVector(x, y);		
-	}
 
+		return new PVector(centerX + x, centerY + y);
+	}
 }
--- src/wordcram/WaveWordPlacer.java
+++ src/wordcram/WaveWordPlacer.java
@@ -1,51 +1,41 @@
 package wordcram;
 
 /*
-Copyright 2010 Daniel Bernier
+ Copyright 2010 Daniel Bernier
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 import processing.core.*;
 
 public class WaveWordPlacer implements WordPlacer, PConstants {
 
-	private java.util.Random r = new java.util.Random();
-	
 	@Override
-	public PVector place(Word word, int wordIndex, int wordsCount, int wordImageWidth, int wordImageHeight, int fieldWidth, int fieldHeight) {
-		return new PVector(getX(fieldWidth, wordIndex, wordsCount), 
-							getY(fieldHeight, wordIndex, wordsCount));
-	}
-	
-	private int getX(int fieldWidth, int wordIndex, int wordsCount) {
-		return PApplet.round(
-					PApplet.map(wordIndex, 0, wordsCount, 0, fieldWidth) 
-					//+ (float)(r.nextGaussian() * 20)
-				);		
-	}
-	
-	private int getY(int fieldHeight, int wordIndex, int wordsCount) {
+	public PVector place(Word word, int wordIndex, int wordsCount,
+			int wordImageWidth, int wordImageHeight, int fieldWidth,
+			int fieldHeight) {
 
-		float sinOffset = PApplet.map((float)Math.sin(PApplet.map(wordIndex, 0, wordsCount, PI, -PI)),
-				0, 1, 0, fieldHeight/3);
-//		sinOffset = 0.0f;
+		float normalizedIndex = (float) wordIndex / wordsCount;
+		float x = normalizedIndex * fieldWidth;
+		float y = normalizedIndex * fieldHeight;
 
-		return PApplet.round(
-					PApplet.map(wordIndex, 0, wordsCount, 0, fieldHeight) 
-					//+ (float)(r.nextGaussian() * 20) 
-					+ sinOffset
-				);		
+		float yOffset = getYOffset(wordIndex, wordsCount, fieldHeight);
+		return new PVector(x, y + yOffset);
 	}
 
+	private float getYOffset(int wordIndex, int wordsCount, int fieldHeight) {
+		float theta = PApplet.map(wordIndex, 0, wordsCount, PI, -PI);
+
+		return (float) Math.sin(theta) * (fieldHeight / 3f);
+	}
 }
