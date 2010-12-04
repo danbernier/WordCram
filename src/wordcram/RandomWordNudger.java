4217d40ced8e1a591ab2e791acc94967aad8237c
danbernier <danbernier@db2b9112-8060-c212-668a-ea1c16feaf2a>
2010-12-04 06:02:03 +0000

    finished the javadoc for RandomWordNudger

--- src/wordcram/RandomWordNudger.java
+++ src/wordcram/RandomWordNudger.java
@@ -20,18 +20,29 @@ import java.util.Random;
 
 import processing.core.PVector;
 
+/**
+ * A RandomWordNudger, where each attempt's PVector has X & Y coords
+ * distributed randomly around the desired point, multiplied by a standard deviation,
+ * and multiplied by the attempt number (so it gets farther, as it gets more 
+ * desperate).
+ * 
+ * @author Dan Bernier
+ */
 public class RandomWordNudger implements WordNudger {
 
 	private Random r = new Random();
 	private float stdDev;
 	
 	/**
-	 * Create a RandomWordNudger, where each attempt
+	 * Create a RandomWordNudger with a standard deviation of 0.6.
 	 */
 	public RandomWordNudger() {
 		this(0.6f);
 	}
 
+	/**
+	 * Create a RandomWordNudger with your own standard deviation.
+	 */
 	public RandomWordNudger(float stdDev) {
 		this.stdDev = stdDev;
 	}
