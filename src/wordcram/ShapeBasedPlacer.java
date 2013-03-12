19c2bfb16fad334dbeca778546726de3b993e4cc
Dan Bernier <danbernier@gmail.com>
2013-03-11 20:41:22 -0400

    Extract ImageShaper from ShapeBasedPlacer

--- src/wordcram/ShapeBasedPlacer.java
+++ src/wordcram/ShapeBasedPlacer.java
@@ -2,8 +2,6 @@ package wordcram;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.font.FontRenderContext;
 import java.awt.font.GlyphVector;
@@ -18,9 +16,8 @@ import javax.imageio.ImageIO;
 
 import processing.core.PVector;
 
-class ShapeBasedPlacer implements WordPlacer, WordNudger {
+public class ShapeBasedPlacer implements WordPlacer, WordNudger {
 
-	public static int TOLERANCE = 5;
 	public static boolean PRECISE = false;
 	public static int GLYPH_SIZE = 500;
 	public static int FONT_STYLE = Font.BOLD;
@@ -31,14 +28,9 @@ class ShapeBasedPlacer implements WordPlacer, WordNudger {
 	float maxX;
 	float maxY;
 	Random random;
-	BufferedImage image = null;
 
 	public ShapeBasedPlacer(Shape shape) {
 		this.area = new Area(shape);
-		init();
-	}
-
-	private void init() {
 		random = new Random();
 		Rectangle2D areaBounds = area.getBounds2D();
 		this.minX = (float) areaBounds.getMinX();
@@ -46,7 +38,7 @@ class ShapeBasedPlacer implements WordPlacer, WordNudger {
 		this.maxX = (float) areaBounds.getMaxX();
 		this.maxY = (float) areaBounds.getMaxY();
 	}
-	
+
 	public static ShapeBasedPlacer fromTextGlyphs(String text, String fontName) {
 		Font font = new Font(fontName, FONT_STYLE, GLYPH_SIZE);
 		BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
@@ -55,70 +47,18 @@ class ShapeBasedPlacer implements WordPlacer, WordNudger {
 		return new ShapeBasedPlacer(glyphVector.getOutline(GLYPH_SIZE / 10, GLYPH_SIZE));
 	}
 	
-	private ShapeBasedPlacer(BufferedImage image) {
-		this.image = image;
-	}
-
-	public static ShapeBasedPlacer fromFile(String path, Color color) {
+	public static ShapeBasedPlacer fromImageFile(String imageFilePath, Color color) {
 		BufferedImage image = null;
 		try {
-			image = ImageIO.read(new File(path));
+			image = ImageIO.read(new File(imageFilePath));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		ShapeBasedPlacer result = new ShapeBasedPlacer(image);
-		if (PRECISE) {
-			result.fromImagePrecise(color);
-		} else {
-			result.fromImageSloppy(color);
-		}
-		result.init();
-		return result;
-	}
-
-	private void fromImagePrecise(Color color) {
-		Area area = new Area();
-		for (int x = 0; x < image.getWidth(); x++) {
-			for (int y = 0; y < image.getHeight(); y++) {
-				Color pixel = new Color(image.getRGB(x, y));
-				if (isIncluded(color, pixel)) {
-					Rectangle r = new Rectangle(x, y, 1, 1);
-					area.add(new Area(r));
-				}
-			}
-		}
-		this.area = area;
-	}
-
-	private void fromImageSloppy(Color color) {
-		Area area = new Area();
-		Rectangle r;
-		int y1, y2;
-		for (int x = 0; x < image.getWidth(); x++) {
-			y1 = 99;
-			y2 = -1;
-			for (int y = 0; y < image.getHeight(); y++) {
-				Color pixel = new Color(image.getRGB(x, y));
-				if (isIncluded(color, pixel)) {
-					if (y1 == 99) {
-						y1 = y;
-						y2 = y;
-					}
-					if (y > (y2 + 1)) {
-						r = new Rectangle(x, y1, 1, y2 - y1);
-						area.add(new Area(r));
-						y1 = y;
-						y2 = y;
-					}
-					y2 = y;
-				}
-			}
-			if ((y2 - y1) >= 0) {
-				r = new Rectangle(x, y1, 1, y2 - y1);
-				area.add(new Area(r));
-			}
-		}
-		this.area = area;
+		
+		ImageShaper shaper = new ImageShaper();
+		Shape shape = PRECISE ? shaper.shapePrecisely(image, color) : shaper.shapeSloppily(image, color);
+		
+		return new ShapeBasedPlacer(shape);
 	}
 
 	public PVector place(Word w, int rank, int count, int ww, int wh, int fw,
@@ -159,17 +99,5 @@ class ShapeBasedPlacer implements WordPlacer, WordNudger {
 
 	float randomBetween(float a, float b) {
 		return a + random.nextFloat() * (b - a);
-	}
-
-	boolean isIncluded(Color target, Color pixel) {
-		int rT = target.getRed();
-		int gT = target.getGreen();
-		int bT = target.getBlue();
-		int rP = pixel.getRed();
-		int gP = pixel.getGreen();
-		int bP = pixel.getBlue();
-		return ((rP - TOLERANCE <= rT) && (rT <= rP + TOLERANCE)
-				&& (gP - TOLERANCE <= gT) && (gT <= gP + TOLERANCE)
-				&& (bP - TOLERANCE <= bT) && (bT <= bP + TOLERANCE));
 	}
 }
