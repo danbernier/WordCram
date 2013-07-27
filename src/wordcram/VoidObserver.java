f0eb34af33244bc8f4df5f123015db0da3169be0
Dan Bernier <danbernier@gmail.com>
2013-07-27 09:32:27 -0400

    Make VoidObserver package-visible, not public (and strip extra whitespace)

--- src/wordcram/VoidObserver.java
+++ src/wordcram/VoidObserver.java
@@ -1,26 +1,11 @@
 package wordcram;
 
-public class VoidObserver implements Observer {
-
-	public void beginDraw() {
-	}
-
-	public void wordDrawn(Word word) {
-	}
-
-	public void wordSkipped(Word word) {
-	}
-
-	public void wordsCounted(Word[] words) {
-	}
-
-	public void wordsScaled(Word[] words) {
-	}
-
-	public void wordsShaped() {
-	}
-
-	public void endDraw(Word[] words) {
-	}
-
+class VoidObserver implements Observer {
+	public void beginDraw() {}
+	public void wordDrawn(Word word) {}
+	public void wordSkipped(Word word) {}
+	public void wordsCounted(Word[] words) {}
+	public void wordsScaled(Word[] words) {}
+	public void wordsShaped() {}
+	public void endDraw(Word[] words) {}
 }
