60f11b486427ec0947b4f0409cae545129e2913f
Dan Bernier <danbernier@gmail.com>
2013-07-27 08:59:17 -0400

    Fix javadoc links in WordSkipReason

--- src/wordcram/WordSkipReason.java
+++ src/wordcram/WordSkipReason.java
@@ -3,7 +3,7 @@ package wordcram;
 public enum WordSkipReason {
 	
 	/**
-     * Skip Reason: the Word was skipped because {@link #maxNumberOfWordsToDraw(int)}
+     * Skip Reason: the Word was skipped because {@link WordCram#maxNumberOfWordsToDraw(int)}
      * was set to some value, and the Word came in over that limit.
      * It's really about the Word's rank, its position in the list once the words are
      * sorted by weight: if its rank is greater than the value passed to maxNumberOfWordsToDraw(),
@@ -14,14 +14,14 @@ public enum WordSkipReason {
 	/**
      * Skip Reason: the Word's shape was too small. WordCram will only render
      * words so small, for performance reasons. You can set the minimum Word shape
-     * size via {@link #minShapeSize(int)}.
+     * size via {@link WordCram#minShapeSize(int)}.
      */
     SHAPE_TOO_SMALL("shape too small"),
     
     /**
      * Skip Reason: WordCram tried placing the Word, but it couldn't find a clear
      * spot. The {@link WordNudger} nudged it around a bunch (according to
-     * {@link #maxAttemptsToPlaceWord(int)}, if it was set), but there was just no room.
+     * {@link WordCram#maxAttemptsToPlaceWord(int)}, if it was set), but there was just no room.
      */
     NO_SPACE("no space");
 	
