8c2a6e20ad5a7608908221175839148e19785a25
Dan Bernier <danbernier@gmail.com>
2013-07-27 09:17:33 -0400

    Update WordSkipReason reasons for clarity

--- src/wordcram/WordSkipReason.java
+++ src/wordcram/WordSkipReason.java
@@ -9,21 +9,21 @@ public enum WordSkipReason {
      * sorted by weight: if its rank is greater than the value passed to maxNumberOfWordsToDraw(),
      * then it'll be skipped, and this will be the reason code.
      */
-	WAS_OVER_MAX_NUMBER_OF_WORDS("number of maximum words reached"),
+	WAS_OVER_MAX_NUMBER_OF_WORDS("we already reached the maxNumberOfWordsToDraw threshold"),
 	
 	/**
      * Skip Reason: the Word's shape was too small. WordCram will only render
      * words so small, for performance reasons. You can set the minimum Word shape
      * size via {@link WordCram#minShapeSize(int)}.
      */
-    SHAPE_WAS_TOO_SMALL("shape too small"),
+    SHAPE_WAS_TOO_SMALL("it was below the minShapeSize threshold"),
     
     /**
      * Skip Reason: WordCram tried placing the Word, but it couldn't find a clear
      * spot. The {@link WordNudger} nudged it around a bunch (according to
      * {@link WordCram#maxAttemptsToPlaceWord(int)}, if it was set), but there was just no room.
      */
-    NO_SPACE("no space");
+    NO_SPACE("there wasn't enough space near where you wanted it placed");
 
     String reason;
 
